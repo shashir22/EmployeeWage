fd0e7b63f4f151c15b20964624d0348cb4f76b66
Shashikumar <shashirgowdas67@gmail.com>
2022-02-11 19:29:44 +0530

    Added:Employee part time calculated

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EmployeeAttandance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HR = 20;
+            int empHr = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+                empHr = 8;
+            else if (empCheck == IS_PART_TIME)
+                empHr = 4;
+            else
+                empHr = 0;
+
+            empWage = empHr * EMP_RATE_PER_HR;
+            Console.WriteLine("The daily wage of employee is :" + empWage);
+
+        }
+    }
+}
